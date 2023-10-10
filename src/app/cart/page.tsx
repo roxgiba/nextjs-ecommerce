ac2371a605d72509f2f7c95db5dcb63db1b711d7
roxgiba <rox.giba@gmail.com>
2023-10-10 16:01:34 +0200

    cart page created

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CartPage() {
+  return (
+    <div>
+      <h1 className="mb-6 text-3xl font-bold">Shopping Cart</h1>
+    </div>
+  )
+}
+
+export default CartPage
