e8c61b7111971d238cb814c9d0e02b73de9651d5
roxgiba <rox.giba@gmail.com>
2023-10-10 15:25:59 +0200

    logo and search implemented

--- src/app/Navabr/Navbar.tsx
+++ src/app/Navabr/Navbar.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/assets/logo.png"
+import { redirect } from "next/navigation";
+
+
+async function searchProducts(formData : FormData) {
+  "use server";
+
+  const searchQuery = formData.get("searchQuery")?.toString();
+
+  if(searchQuery) {
+    redirect("/search?query=" + searchQuery)
+  }
+}
+
 export default function Navbar(){
   return(
     <div className="bg-base-100">
+      <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl normal-case">
+            <Image  src={logo} width={40} height={40} alt="NatureList logo" />
+            NatureList
+          </Link>
+        </div>
+        <div className="flex-none gap-2">
+          <form action={searchProducts}>
+            <div className="form-control">
+              <input 
+              name="searchQuesty"
+              placeholder="Search"
+              className="input input-bordered w-full min-w-[100px]"
+              />
 
+            </div>
+
+          </form>
+
+        </div>
+      </div>
     </div>
   )
 }
