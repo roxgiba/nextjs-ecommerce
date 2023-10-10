701c4665cab5d96b45e406fbfd97481241514f77
roxgiba <rox.giba@gmail.com>
2023-10-10 15:47:22 +0200

    footer in layout implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './Navabr/Navbar'
+import Footer from './Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           <main className='p-4 max-w-7xl m-auto min-w-[300px]'>
           {children}
           </main>
+          <Footer />
         </body>
     </html>
   )
