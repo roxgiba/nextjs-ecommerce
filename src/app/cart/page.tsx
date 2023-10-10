fcf46d0ebdf08a0afa049212055ef8755719c3d9
roxgiba <rox.giba@gmail.com>
2023-10-10 16:56:40 +0200

    subtotal and checkout button created

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,6 +2,7 @@ import { getCart } from '@/lib/db/cart'
 import React from 'react'
 import CartEntry from './CartEntry';
 import setProductQuantity from "./actions"
+import { formatPrice } from '@/lib/format';
 
 export const metadata = {
   title: "Your Cart - NatureList "
@@ -17,6 +18,10 @@ const cart = await getCart();
         <CartEntry cartItem={cartItem} key={cartItem.id} setProductQuantity={setProductQuantity} />
       ))}
       {!cart?.items.length && <p>Your cart is empty.</p>}
+      <div className='flex flex-col items-end sm:items-center'>
+        <p className='mb-3 font-bold'> Total: {formatPrice(cart?.subtotal || 0)}</p>
+        <button className='btn btn-primary sm:w-[200px]'>Checkout</button>
+      </div>
     </div>
   )
 }
