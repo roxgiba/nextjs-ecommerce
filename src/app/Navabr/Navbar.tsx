85cc2889fdc2be896b813a008a7b7c334b1df5de
roxgiba <rox.giba@gmail.com>
2023-10-25 11:24:43 +0200

    sign in/out button added to navbar

--- src/app/Navabr/Navbar.tsx
+++ src/app/Navabr/Navbar.tsx
@@ -4,6 +4,9 @@ import logo from "@/assets/logo.png"
 import { redirect } from "next/navigation";
 import { getCart } from "@/lib/db/cart";
 import ShoppingCartButton from "./ShoppingCartButton";
+import UserMenuButton from "./UserMenuButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 
 async function searchProducts(formData : FormData) {
@@ -17,7 +20,7 @@ async function searchProducts(formData : FormData) {
 }
 
 export default async function Navbar(){
-
+  const session = await getServerSession(authOptions);
   const cart = await getCart();
 
 
@@ -43,6 +46,7 @@ export default async function Navbar(){
 
           </form>
           <ShoppingCartButton cart={cart} />
+          <UserMenuButton session={session} />
         </div>
       </div>
     </div>
