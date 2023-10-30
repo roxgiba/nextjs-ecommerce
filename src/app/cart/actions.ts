daf8956f05c5490cf6ee56cec35ca4fcb210c12a
roxgiba <rox.giba@gmail.com>
2023-10-30 15:31:37 +0100

    fixing typo

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -4,7 +4,7 @@ import { createCart, getCart } from "@/lib/db/cart";
 import { prisma } from "@/lib/db/prisma";
 import { revalidatePath } from "next/cache";
 
-export async function incrementProductQuantity(productId: string) {
+export default async function incrementProductQuantity(productId: string) {
   const cart = (await getCart()) ?? (await createCart());
 
   const articleInCart = cart.items.find((item) => item.productId === productId);
