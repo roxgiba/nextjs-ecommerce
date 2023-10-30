be002c06007ecdac5443d713c18f917f50e141fa
roxgiba <rox.giba@gmail.com>
2023-10-30 14:28:51 +0100

    pagination and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,34 @@ import { prisma } from '@/lib/db/prisma'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default async function Home() {
+interface HomeProps {
+  searchParams: {
+    page: string
+  }
+}
+
+export default async function Home({searchParams: {page = "1"}} : HomeProps) {
+  const currentPage = parseInt(page)
+
+  const pageSize = 6 
+  const heroItemCount = 1
+
+  const totalItemCount = await prisma.product.count()
+
+  const totalPages = Math.ceil((totalItemCount - heroItemCount) / pageSize)
+
+
+
   const products = await prisma.product.findMany({
     orderBy:{
       id: "desc"
-    } 
+    },
+    skip: (currentPage - 1) * pageSize + (currentPage === 1 ? 0 : heroItemCount),
+    take: pageSize + (currentPage === 1 ? heroItemCount : 0)
   })
   return (
     <div className='flex flex-col items-center'>
+      {currentPage === 1 && (
      <div className='hero - rounded-xl bg-base-200'>
      <div className="hero-content flex-col lg:flex-row">
             <Image
@@ -31,14 +51,17 @@ export default async function Home() {
       </div>
       </div>
      </div>
+      )}
 
-     <div className='my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
-      {products.slice(1).map(product => (
-        <ProductCard product={product} key={product.id}/>
-      ))}
+      <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+        {(currentPage === 1 ? products.slice(1) : products).map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
+      </div>
 
-     </div>
-     <PaginationBar currentPage={13} totalPages={99} />
+     {totalPages > 1 && ( 
+     <PaginationBar currentPage={currentPage} totalPages={totalPages} />
+     )}
     </div>
-  )
+  );
 }
