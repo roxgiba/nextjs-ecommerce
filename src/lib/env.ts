7185d39f3ac511d7861d7846d22a0fecfe03d48c
roxgiba <rox.giba@gmail.com>
2023-11-07 09:10:56 +0100

    dotenv added to lib

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,11 +1,15 @@
 import zod from "zod";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env
+dotenv.config();
 
 const envSchema = zod.object({
-DATABASE_URL: zod.string().nonempty(),
-GOOGLE_CLIENT_ID: zod.string().nonempty(),
-GOOGLE_CLIENT_SECRET: zod.string().nonempty(),
-NEXTAUTH_URL: zod.string().nonempty(),
-NEXTAUTH_SECRET: zod.string().nonempty(),
-})
+  DATABASE_URL: zod.string().nonempty(),
+  GOOGLE_CLIENT_ID: zod.string().nonempty(),
+  GOOGLE_CLIENT_SECRET: zod.string().nonempty(),
+  NEXTAUTH_URL: zod.string().nonempty(),
+  NEXTAUTH_SECRET: zod.string().nonempty(),
+});
 
 export const env = envSchema.parse(process.env);
